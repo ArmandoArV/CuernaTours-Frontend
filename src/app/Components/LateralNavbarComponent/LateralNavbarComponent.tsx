d648c86d27ee83228eb41e4186c75c0843711a53
ArmandoArV <armando.arredondo.valle@gmail.com>
2025-09-30 13:59:05 -0600

    implementation of lateralnavbar and responsiveness

--- src/app/Components/LateralNavbarComponent/LateralNavbarComponent.tsx
+++ src/app/Components/LateralNavbarComponent/LateralNavbarComponent.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import styles from "./LateralNavbar.module.css";
 import Link from "next/link";
 import Image from "next/image";
@@ -10,6 +11,7 @@ type Props = {
   userIsOwner?: boolean;
   openLogo?: string;
   closedLogo?: string;
+  onMobileMenuToggle?: (isOpen: boolean) => void;
 };
 
 const LateralNavbarComponent: React.FC<Props> = ({
@@ -18,7 +20,15 @@ const LateralNavbarComponent: React.FC<Props> = ({
   userIsOwner = false,
   openLogo = "/Images/CuernaToursLogo.svg",
   closedLogo = "/Images/CuernaToursAsset1.svg",
+  onMobileMenuToggle,
 }) => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const handleMobileToggle = () => {
+    const newState = !isMobileMenuOpen;
+    setIsMobileMenuOpen(newState);
+    onMobileMenuToggle?.(newState);
+  };
   // Filter items based on user role
   const filteredItems = items.filter((item) => {
     // If user is owner, they can see all items
@@ -36,41 +46,62 @@ const LateralNavbarComponent: React.FC<Props> = ({
   });
 
   return (
-    <nav className={styles.lateralNavbar}>
-      {/* Logo Section */}
-      <div className={styles.logoSection}>
-        <Image
-          src={closedLogo}
-          alt="Cuerna Tours Logo"
-          width={40}
-          height={40}
-          className={styles.logoDefault}
-        />
-        <Image
-          src={openLogo}
-          alt="Cuerna Tours Logo"
-          width={150}
-          height={60}
-          className={styles.logoExpanded}
-        />
-      </div>
+    <>
+      {/* Mobile Hamburger Button */}
+      <button 
+        className={styles.mobileHamburger}
+        onClick={handleMobileToggle}
+        aria-label="Toggle mobile menu"
+      >
+        <span className={`${styles.hamburgerLine} ${isMobileMenuOpen ? styles.hamburgerLineOpen : ''}`}></span>
+        <span className={`${styles.hamburgerLine} ${isMobileMenuOpen ? styles.hamburgerLineOpen : ''}`}></span>
+        <span className={`${styles.hamburgerLine} ${isMobileMenuOpen ? styles.hamburgerLineOpen : ''}`}></span>
+      </button>
 
-      {/* Navigation Items */}
-      <ul className={styles.navList}>
-        {filteredItems.map((item, index) => (
-          <li key={index} className={styles.navItem}>
-            <Link href={item.link} className={styles.navLink}>
-              {item.icon && (
-                <span className={styles.icon}>
-                  {item.icon}
-                </span>
-              )}
-              <span className={styles.title}>{item.title}</span>
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </nav>
+      {/* Mobile Overlay */}
+      {isMobileMenuOpen && <div className={styles.mobileOverlay} onClick={handleMobileToggle}></div>}
+
+      {/* Navigation */}
+      <nav className={`${styles.lateralNavbar} ${isMobileMenuOpen ? styles.mobileOpen : ''}`}>
+        {/* Logo Section */}
+        <div className={styles.logoSection}>
+          <Image
+            src={closedLogo}
+            alt="Cuerna Tours Logo"
+            width={40}
+            height={40}
+            className={styles.logoDefault}
+          />
+          <Image
+            src={openLogo}
+            alt="Cuerna Tours Logo"
+            width={150}
+            height={60}
+            className={styles.logoExpanded}
+          />
+        </div>
+
+        {/* Navigation Items */}
+        <ul className={styles.navList}>
+          {filteredItems.map((item, index) => (
+            <li key={index} className={styles.navItem}>
+              <Link 
+                href={item.link} 
+                className={styles.navLink}
+                onClick={() => setIsMobileMenuOpen(false)}
+              >
+                {item.icon && (
+                  <span className={styles.icon}>
+                    {item.icon}
+                  </span>
+                )}
+                <span className={styles.title}>{item.title}</span>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </>
   );
 };
 export default LateralNavbarComponent;
