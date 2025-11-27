17f4d5070859fda413511f9b53f9540fb14e26f7
ArmandoArV <armando.arredondo.valle@gmail.com>
2025-11-26 20:53:11 -0600

    Implements loading

---
+++ src/app/dashboard/loading.tsx
@@ -0,0 +1,5 @@
+import LoadingComponent from "@/app/Components/LoadingComponent/LoadingComponent";
+
+export default function Loading() {
+  return <LoadingComponent message="Cargando panel de control..." />;
+}
